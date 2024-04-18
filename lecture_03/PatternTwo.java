ffeffae0ac46f72f59ce1fd08306feccde35a372
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-18 15:43:53 +0530

    patter print

--- lecture_03/PatternTwo.java
+++ lecture_03/PatternTwo.java
@@ -9,7 +9,9 @@ public class PatternTwo {
     int n = in.nextInt();
     for(int i=1; i<=n;i++){
         for(int j=1;j<=i;j++){
-            System.out.print("*");
+            // System.out.print("*");
+            // System.out.print(i);
+            System.out.print(j);
         }
         System.out.println();
     }
