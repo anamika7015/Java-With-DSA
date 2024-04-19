5ee2780d23e074800f7fea094390990b65040a9a
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-19 18:33:09 +0530

    update

--- lecture_03/PatternTwo.java
+++ lecture_03/PatternTwo.java
@@ -7,13 +7,14 @@ public class PatternTwo {
     System.out.print("Enter the number");
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    // char ch ='A';
+    char ch ='A';
     for(int i=1; i<=n;i++){
         for(int j=1;j<=i;j++){
             // System.out.print("*");
             // System.out.print(i);
-            System.out.print(j);
+            System.out.print(ch);
         }
+        ch++;
         System.out.println();
     }
     }
