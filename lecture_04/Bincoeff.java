ac1c8b380f8f0f8b93f07f5044e7cd46574f61c1
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-21 21:19:48 +0530

    Create Bincoeff.java

---
+++ lecture_04/Bincoeff.java
@@ -0,0 +1,9 @@
+package lecture_04;
+import java.util.Scanner;
+
+public class Bincoeff {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int Fact
+    }
+}
