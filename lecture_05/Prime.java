3ae71fbd3b33e3edd954a4a5a3c6bf6f1a50cbb7
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-22 19:59:23 +0530

    prime number or not

---
+++ lecture_05/Prime.java
@@ -0,0 +1,18 @@
+package lecture_05;
+
+public class Prime {
+    public static boolean isPrime(int n){
+        boolean isPrime = true;
+        for(int i=2; i<=n-1;i++){
+            if(n%i == 2){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+    public static void main(String[]
+     args) {
+        System.out.println(isPrime(3));
+    }
+}
