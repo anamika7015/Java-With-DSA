3b2f937c951ae0e586b688109352435458e8c810
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-22 19:31:16 +0530

    updated program

--- lecture_04/Bincoeff.java
+++ lecture_04/Bincoeff.java
@@ -2,25 +2,28 @@ package lecture_04;
 import java.util.Scanner;
 
 
-public static void factorial(int n){
-int f=1;
-for( int i=1;i<=n;i++){
-    f= f*i;
 
-}
-return f;
-}
-public static void bincoeff(int n, int r){
-    int fact_n = factorial(n);
-    int fact_r = factorial(r);
-    int fact_nmr = factorial(n-r);
-    int Bincoeff = fact_n/(fact_r*fact_nmr);
-    return bincoeff;
-}
+
+
 
 public class Bincoeff {
+    public static int bincoeff(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+        int Bincoeff = fact_n/(fact_r*fact_nmr);
+        return Bincoeff;
+    }
+    public static int factorial(int num) {
+        if (num == 0) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+    
     public static void main(String[] args) {
-       System.out.println(facorial(4));
+       
      System.out.println(bincoeff(5,2));
     }
 }
