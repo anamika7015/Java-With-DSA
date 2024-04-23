10acb654ab25fc5310f2315164a91609e57a6d94
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-23 15:07:55 +0530

    update

--- lecture_05/BinTodec.java
+++ lecture_05/BinTodec.java
@@ -1,24 +1,23 @@
 package lecture_05;
 
 public class BinToDec {
-public static void binToDec(int binNum){
-    int mynum = binNum ;
-    int pow = 0;
-    int decNum = 0;
-    while(binNum>0){
-        int lastDigit = binNum%10;
-        decNum  = decNum+(lastDigit+(int)Math.pow(2,pow));
-        pow++;
-        binNum =binNum/10;
-    }
-        System.out.println("decimal of "+mynum+"="decNum);
+    public static void binToDec(int binNum) {
+        int mynum = binNum;
+        int pow = 0;
+        int decNum = 0;
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int) Math.pow(2, pow));
+            System.out.println(decNum);
+            pow++;
+            binNum = binNum / 10;
         }
-        public static void main(String args[]){
+        System.out.println("decimal of " + mynum + "=" + decNum);
+    }
+
+    public static void main(String args[]) {
         binToDec(111);
 
-
-
-
-}
+    }
 
 }
