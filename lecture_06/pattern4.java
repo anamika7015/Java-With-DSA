03f4de80b399e32f5c513f7843686b021eedca13
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-23 18:42:24 +0530

    floyd triangle

--- lecture_06/pattern4.java
+++ lecture_06/pattern4.java
@@ -1,5 +1,18 @@
 package lecture_06;
 
 public class pattern4 {
-    
+    public static void  floyds_triangle(int n){
+      int counter =1;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print(counter+" ");
+        counter++;
+    }
+    System.out.println();
+}
+
+    }
+    public static void main(String[] args) {
+        floyds_triangle(5);
+    }
 }
