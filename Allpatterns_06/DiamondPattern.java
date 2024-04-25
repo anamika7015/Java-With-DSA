09374fd7c41dc09f65715f2eb3ab66823b634aa9
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-25 15:28:40 +0530

    update diamond

--- Allpatterns_06/DiamondPattern.java
+++ Allpatterns_06/DiamondPattern.java
@@ -13,7 +13,7 @@ public class DiamondPattern {
 
             
         }
-        for (int i = n; i >= 1; i--) {
+        for (int i = n - 1; i >= 1; i--) {
         for (int j = 1; j <= n - i; j++) {
             System.out.print(" ");
         }
