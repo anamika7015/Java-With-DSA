5574e992217c3507fc2aec868bca6edb9aba8bb6
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 21:20:57 +0530

    binary search

---
+++ lecture_06/Binnumbers.java
@@ -0,0 +1,11 @@
+package lecture_06;
+
+public class Binnumbers {
+    public static int binaryNumber(int number[],int key){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
