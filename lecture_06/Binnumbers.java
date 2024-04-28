7b891f8d7930260efdd58881a67d2c592f7b8978
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 21:34:50 +0530

    binary search

--- lecture_06/Binnumbers.java
+++ lecture_06/Binnumbers.java
@@ -1,10 +1,28 @@
 package lecture_06;
 
 public class Binnumbers {
-    public static int binaryNumber(int number[],int key){
+    public static int binarySearch(int number[],int key){
+        int start = 0, end = number.length-1;
+        while(start<=end){
+            int mid = (start + end) / 2;
+            if( number[mid] == key){
+                return mid;
+
+            }
+            if(number[mid] < key){
+                start = mid +1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
 
     }
     public static void main(String[] args) {
+        int number[] = {2,4,6,8,10,12,14,16,17};
+        int key = 16;
+        System.out.println("index for key is:"+ binarySearch(number, key));
         
     }
     
