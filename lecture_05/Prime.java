0088d209534a69064be07371c4e8670be8626ac3
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 06:19:30 +0530

    print sub array

--- lecture_05/Prime.java
+++ lecture_05/Prime.java
@@ -2,31 +2,25 @@ package lecture_05;
 
 public class Prime {
 
-
-
-
-    public static boolean isPrime(int n){
+    public static boolean isPrime(int n) {
         boolean isPrime = true;
-        for(int i=2; i<=n-1;i++){
-            if(n%i == 2){
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 2) {
                 isPrime = false;
                 break;
             }
         }
         return isPrime;
     }
-    public static boolean primeInRange(int n){
-        for(int i=1; i<=n;i++){
-           is(isPrime(i+" ")){
-           System.out.println();
-           }
-        }
-        System.out.println();
 
-    }
-    public static void main(String[]
-     args) {
+    // public static boolean primeInRange(int n){
+    //     for(int i=1; i<=n;i++){
+    //        (isPrime(i+" ")){
+    //        System.out.println();
+    //        }
+     
+    public static void main(String[] args) {
         // System.out.println(isPrime(3));
-        primeInRange(100);
+      //  primeInRange(100);
     }
 }
