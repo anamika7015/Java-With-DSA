9cf04774ed50df8cf196c3550c538f10f87d721a
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-08 15:08:34 +0530

    bubble sort

--- lecture_07/Bubblesort.java
+++ lecture_07/Bubblesort.java
@@ -14,15 +14,16 @@ public class Bubblesort {
                 }
             }
         }
+    }
         public static void printArr(int arr[]){
-            for(int i=0;i<n;i++){
+            for(int i=0;i<arr.length;i++){
                 System.out.print(arr[i]+" ");
             }
             System.out.println();
          }
         
 
-    }
+    
 
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
