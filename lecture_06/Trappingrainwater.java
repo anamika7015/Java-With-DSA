792cfbe9c5f8c7fd38c29661422d10626672c111
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-08 14:54:58 +0530

    trapping rain water

--- lecture_06/Trappingrainwater.java
+++ lecture_06/Trappingrainwater.java
@@ -1,20 +1,20 @@
 package lecture_06;
-import java.util.Scanner;
+
 
 public class Trappingrainwater {
     public static int Trapping_Rain_Water(int height[]) {
         int n = height.length;
         // calculate left max boundary - array
-        int leftMax = new int[n];
+        int leftMax[] = new int[n];
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
 
         }
-        int rightMax = new int[n];
+        int rightMax[] = new int[n];
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            leftMax[i] = Math.max(height[i], rightMax[i + 1]);
+            leftMax[i] = Math.max(height[i], rightMax[i+1]);
 
         }
         int trappedWater = 0;
