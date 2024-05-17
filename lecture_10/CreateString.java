331ecc79f21e16100f695bd4347d09b50c54d09e
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-17 16:05:48 +0530

    creation of string

---
+++ lecture_10/CreateString.java
@@ -0,0 +1,13 @@
+package lecture_10;
+
+public class CreateString {
+    public static void main(String[] args) {
+        // String str = new String();
+        // str = "anamika pandey";
+        String str = "anamika pandey";
+
+        for(int i = 0; i< str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
